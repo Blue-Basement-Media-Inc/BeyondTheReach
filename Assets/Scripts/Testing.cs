f61737ebcb3cd9044489073d921fc215ac2155c8
Code Monkey <admin@unitycodemonkey.com>
2022-05-27 19:01:04 +0100

    16. Grid Object

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Testing : MonoBehaviour
 {
 
+    [SerializeField] private Transform gridDebugObjectPrefab;
+
+
     private GridSystem gridSystem;
+
 
     private void Start()
     {
         gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
 
         Debug.Log(new GridPosition(5, 7));
     }
